d1de7a208a02034bef32499a4ec64801dac640d5
ShaunDyson <109430979+ShaunDyson@users.noreply.github.com>
2022-12-01 17:59:55 -0500

    day1: part2

--- day1/day1.cc
+++ day1/day1.cc
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -11,6 +13,7 @@ int main(int argc, char** argv) {
         return -1;
     }
     int cache_sum = 0, cache_max = INT32_MIN;
+    vector<int> memo;
     for (string line; getline(ifs, line);)
     {
         if (!line.empty())
@@ -19,9 +22,17 @@ int main(int argc, char** argv) {
             continue;
         }
         cache_max = max(cache_max, cache_sum);
+        memo.push_back(cache_sum);
         cache_sum = 0;
     }
     cache_max = max(cache_max, cache_sum);
+    memo.push_back(cache_sum);
+    partial_sort(memo.begin(), next(memo.begin(), 3), memo.end(), greater<int>());
     cout << "the max is " << cache_max << endl;
+    for (int i = 0; i < 3; ++i)
+    {
+        cout << "the top " << i << " is " << memo[i] << endl;
+    }
+    cout << "total is: " << memo[0] + memo[1] + memo[2] << endl;
     return 0;
 }
