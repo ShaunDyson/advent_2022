fccd0352b531db0861a4a66ae84801bbe1f884f7
ShaunDyson <109430979+ShaunDyson@users.noreply.github.com>
2022-12-03 00:39:23 -0500

    day3: part2

--- day3/day3.cc
+++ day3/day3.cc
@@ -12,6 +12,7 @@ int main(int argc, char** argv) {
     if (!ifs.is_open())
         return -1;
     int res1 = 0;
+    int res2 = 0;
     auto f = [](char x) {
         if (x >= 'a' && x <= 'z')
             return x - 'a' + 1;
@@ -20,6 +21,8 @@ int main(int argc, char** argv) {
         assert(0&&"failed");
         return 0;
     };
+    set<char> memo2;
+    int i = 0;
     for(string line; getline(ifs, line);)
     {
         auto len = line.length();
@@ -29,9 +32,34 @@ int main(int argc, char** argv) {
             return memo.count(x);
         });
         assert(it != line.end() && "cannot find it?");
-        cout << "*it is " << *it << " and " << f(*it) << endl;
         res1 += f(*it);
+
+        set<char> memo2_tmp;
+        switch ((++i) % 3)
+        {
+        case 0:
+            copy_if(line.begin(), line.end(), inserter(memo2_tmp, memo2_tmp.begin()), [&memo2](char x) {
+                return memo2.count(x);
+            });
+            assert(memo2_tmp.size() == 1 && "is not the only one?");
+            res2 += f(*memo2_tmp.begin());
+            memo2.clear();
+            break;
+        case 1:
+            copy(line.begin(), line.end(), inserter(memo2, memo2.begin()));
+            break;
+        case 2:
+            copy_if(line.begin(), line.end(), inserter(memo2_tmp, memo2_tmp.begin()), [&memo2](char x) {
+                return memo2.count(x);
+            });
+            memo2 = memo2_tmp;
+            break;
+        default:
+            assert(0&&"cannot go here");
+            break;
+        }    
     }
     cout << "part 1 result: " << res1 << endl;
+    cout << "part 2 result: " << res2 << endl;
     return 0;
 }
