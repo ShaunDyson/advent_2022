dba0b89e815b7a2aff6ae8133276dd20a3e77259
ShaunDyson <109430979+ShaunDyson@users.noreply.github.com>
2022-12-04 00:18:08 -0500

    day4: part2

--- day4/day4.cc
+++ day4/day4.cc
@@ -12,6 +12,7 @@ int main(int argc, char** argv) {
     if (!ifs.is_open())
         return -1;
     int res1 = 0;
+    int res2 = 0;
     for(string line; getline(ifs, line);)
     {
         auto first = line.begin(), last = line.end();
@@ -25,9 +26,24 @@ int main(int argc, char** argv) {
         int j0 = stoi(string(first, first2));
         int j1 = stoi(string(++first2, last));
         if ((i0 <= j0 && j1 <= i1) || (j0 <= i0 && i1 <= j1))
+        {
             ++res1;
+            ++res2;
+            continue;
+        }
+        if ((i0 <= j0 && j0 <= i1) || (i0 <= j1 && j1 <= i1))
+        {
+            ++res2;
+            continue;
+        }
+        if ((j0 <= i0 && i0 <= j1) || (j0 <= i1 && i1 <= j1))
+        {
+            ++res2;
+            continue;
+        }
         //cout << i0 << "," << i1 << "," << j0 << "," << j1 << "," << res1 << endl;
     }
     cout << res1 << endl;
+    cout << res2 << endl;
     return 0;
 }
