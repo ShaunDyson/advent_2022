33f70144082682e0dad6ced4d4410e54322cfcd3
ShaunDyson <109430979+ShaunDyson@users.noreply.github.com>
2022-12-04 00:13:06 -0500

    day4: part1

---
+++ day4/day4.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <assert.h>
+#include <set>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    ifstream ifs("input.txt");
+    if (!ifs.is_open())
+        return -1;
+    int res1 = 0;
+    for(string line; getline(ifs, line);)
+    {
+        auto first = line.begin(), last = line.end();
+        auto first2 = find(first, last, '-');
+        int i0 = stoi(string(first, first2));
+        first = ++first2;
+        first2 = find(first, last, ',');
+        int i1 = stoi(string(first, first2));
+        first = ++first2;
+        first2 = find(first, last, '-');
+        int j0 = stoi(string(first, first2));
+        int j1 = stoi(string(++first2, last));
+        if ((i0 <= j0 && j1 <= i1) || (j0 <= i0 && i1 <= j1))
+            ++res1;
+        //cout << i0 << "," << i1 << "," << j0 << "," << j1 << "," << res1 << endl;
+    }
+    cout << res1 << endl;
+    return 0;
+}
